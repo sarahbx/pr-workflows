6c3ebccc113a8264c5c4a03c0ea135ceb70b8c9c
Meni Yakove <myakove@redhat.com>
2021-08-04 16:32:51 +0300

    ...

--- main.py
+++ main.py
@@ -9,3 +9,5 @@ github = github.Github(token)
 # GITHUB_REPOSITORY is the repo name in owner/name format in Github Workflow
 repo = github.get_repo(os.environ['GITHUB_REPOSITORY'])
 print(os.environ.get("GITHUB_EVENT_PATH"))
+print(os.environ.get("GITHUB_SHA"))
+print(os.environ.get("GITHUB_REF"))
