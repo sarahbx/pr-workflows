4e13f2293f5fd2cd774d6a3d51dee80702121374
azavalko <azavalko@redhat.com>
2021-08-05 14:15:49 +0300

    Relieve main.py of having to check trigger reason

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ import os
 
 import github
 
+ACTIONS = {
+    'labels_by_user_input':labels_by_user_input,
+    'add_reviewers':add_reviewers,
+    'size_label_prs': size_label_prs
+}
 
 def get_labels(pull):
     return [label.name for label in pull.get_labels()]
 
 
-def labels_by_user_input(data, pull):
+def labels_by_user_input(**kwargs):
+    data, pull = kwargs['data'], kwargs['pull']
     body = data["comment"]["body"]
     label = "Verified"
     if "/verified" in body and label not in get_labels(pull=pull):
@@ -27,7 +33,8 @@ def remove_verified_label(pull):
         pull.remove_from_labels(label)
 
 
-def add_reviewers(data, pull, reviewers):
+def add_reviewers(**kwargs):
+    data, pull, reviewers = kwargs['data'], kwargs['pull'], kwargs['reviewers']
     reviewers = [reviewer.strip() for reviewer in reviewers.split(",")]
     author = [data["sender"]["login"]]
     current_reviewers_requests = data["pull_request"]["requested_reviewers"]
@@ -37,7 +44,8 @@ def add_reviewers(data, pull, reviewers):
             pull.create_review_request([reviewer])
 
 
-def size_label_prs(data, pull):
+def size_label_prs(**kwargs):
+    data, pull = kwargs['data'], kwargs['pull']
     labels = get_labels(pull=pull)
     additions = data["pull_request"]["additions"]
     label = None
@@ -67,8 +75,8 @@ def size_label_prs(data, pull):
 
 if __name__ == "__main__":
     token = os.environ["INPUT_TOKEN"]
-    event_type = os.environ["GITHUB_EVENT_NAME"]
-    reviewers = os.environ["INPUT_REVIEWERS"]
+    reviewers = os.environ['INPUT_REVIEWERS']
+    action = os.environ['INPUT_ACTION']
     with open(os.environ.get("GITHUB_EVENT_PATH"), "r") as fd:
         data = json.load(fd)
 
@@ -82,10 +90,4 @@ if __name__ == "__main__":
 
     pull = repo.get_pull(issue_number)
 
-    if event_type == "pull_request_target":
-        remove_verified_label(pull=pull)
-        size_label_prs(data=data, pull=pull)
-        add_reviewers(data=data, pull=pull, reviewers=reviewers)
-
-    if event_type == "issue_comment":
-        labels_by_user_input(data=data, pull=pull)
+    ACTIONS[action](data=data, pull=pull, reviewers=reviewers)
