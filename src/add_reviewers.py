018734f9e5e903fcc41d42d79d6e12a9c4c2f4ed
Meni Yakove <myakove@redhat.com>
2021-08-08 13:59:42 +0300

    add_offensive_lang

--- src/add_reviewers.py
+++ src/add_reviewers.py
@@ -1,8 +1,12 @@
+from github.GithubException import GithubException
+
+
 def add_reviewers(data, pull, reviewers):
     reviewers = [reviewer.strip() for reviewer in reviewers.split(",")]
-    author = [data["sender"]["login"]]
-    current_reviewers_requests = data["pull_request"]["requested_reviewers"]
     for reviewer in reviewers:
-        if reviewer not in current_reviewers_requests + author + [pull.user.login]:
+        try:
             print(f"Requesting review from {reviewer} for {pull.title}")
             pull.create_review_request([reviewer])
+        except GithubException as exp:
+            # author ot the user who pushed cannot be add as reviewers
+            print(f"Failed to add {reviewer}: {exp}")
