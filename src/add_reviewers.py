394daab72c3cadc39645e9daf3ddff204f54e2b0
Meni Yakove <myakove@redhat.com>
2021-08-08 13:51:40 +0300

    add_offensive_lang

--- src/add_reviewers.py
+++ src/add_reviewers.py
@@ -3,6 +3,6 @@ def add_reviewers(data, pull, reviewers):
     author = [data["sender"]["login"]]
     current_reviewers_requests = data["pull_request"]["requested_reviewers"]
     for reviewer in reviewers:
-        if reviewer not in current_reviewers_requests + author + pull.user:
+        if reviewer not in current_reviewers_requests + author + [pull.user.login]:
             print(f"Requesting review from {reviewer} for {pull.title}")
             pull.create_review_request([reviewer])
