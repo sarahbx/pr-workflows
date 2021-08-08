9945091a392649a3372c3290424286f80a834d4f
Meni Yakove <myakove@redhat.com>
2021-08-08 13:25:01 +0300

    add_offensive_lang

--- src/block_offensive_lanague.py
+++ src/block_offensive_lanague.py
@@ -9,11 +9,15 @@ def block_offensive_lanague(pull):
     offensive_words = re.compile("black[ -]?list|white[ -]?list|master|slave")
 
     for _file in pull.get_files():
+        if "offensive_language_actions.yml" in __file__:
+            continue
+
         for idx, line in enumerate(requests.get(_file.raw_url).text.splitlines()):
             match = re.search(offensive_words, line)
             if match:
                 print(
-                    f"file: {_file.filename} line{idx}: found offensive words [{match.group()}]"
+                    f"file: {_file.filename} line: {idx + 1},{match.span()[0]}\n\t"
+                    f"found offensive words [{match.group()}]"
                 )
                 offensive_words_match = True
 
