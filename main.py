1ab653afb661ae03792675a6c46f1ba229d12098
Meni Yakove <myakove@redhat.com>
2021-08-08 13:45:28 +0300

    add_offensive_lang

--- main.py
+++ main.py
@@ -11,14 +11,13 @@ from src.size_label_prs import size_label_prs
 
 if __name__ == "__main__":
     token = os.environ["INPUT_TOKEN"]
-    reviewers = os.environ["INPUT_REVIEWERS"]
     action = os.environ["INPUT_ACTION"]
-    with open(os.environ["GITHUB_EVENT_PATH"], "r") as fd:
-        data = json.load(fd)
-
     github = github.Github(token)
     repo = github.get_repo(os.environ["GITHUB_REPOSITORY"])
     commit = repo.get_commit(os.environ["GITHUB_SHA"])
+    with open(os.environ["GITHUB_EVENT_PATH"], "r") as fd:
+        data = json.load(fd)
+
     try:
         pull = repo.get_pull(data["number"])
     except KeyError:
@@ -31,6 +30,7 @@ if __name__ == "__main__":
         labels_by_user_input(data=data, pull=pull)
 
     elif action == "add_reviewers":
+        reviewers = os.environ["INPUT_REVIEWERS"]
         add_reviewers(data=data, pull=pull, reviewers=reviewers)
 
     elif action == "size_label_prs":
