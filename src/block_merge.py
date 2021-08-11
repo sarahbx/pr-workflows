4ed4019ecd49d34922101563dbb9bc0ced435c80
Meni Yakove <myakove@redhat.com>
2021-08-11 03:26:02 +0300

    Block merge if "Verified" label is missing

--- src/block_merge.py
+++ src/block_merge.py
@@ -1,16 +1,6 @@
-from src.constants import LABEL_VERIFIED
-
-
 def block_merge_no_verify(pull):
     context = "Verified label"
-    labels = pull.get_labels()
     last_commit = list(pull.get_commits())[-1]
-    if LABEL_VERIFIED not in labels:
-        last_commit.create_status(
-            state="pending", description="Missing Verified label", context=context
-        )
-
-    else:
-        last_commit.create_status(
-            state="success", description="Missing Verified label", context=context
-        )
+    last_commit.create_status(
+        state="pending", description="Missing Verified label", context=context
+    )
--- src/labels_by_user_input.py
+++ src/labels_by_user_input.py
@@ -1,5 +1,7 @@
-from src.constants import LABEL_VERIFIED
 from src.utils import get_labels
+
+
+LABEL_VERIFIED = "Verified"
 
 
 def remove_verified_label(pull):
@@ -14,6 +16,12 @@ def labels_by_user_input(data, pull):
     if "/verified" in body and LABEL_VERIFIED not in get_labels(pull=pull):
         print(f"Adding {LABEL_VERIFIED} to {pull.title}")
         pull.add_to_labels(LABEL_VERIFIED)
+        last_commit = list(pull.get_commits())[-1]
+        last_commit.create_status(
+            state="pending",
+            description="Missing Verified label",
+            context="Verified label",
+        )
 
     if "/unverified" in body:
         remove_verified_label(pull=pull)
