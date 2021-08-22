35617282016a5ce4a1fe9bdeb7d2211338675742
Meni Yakove <myakove@redhat.com>
2021-08-22 23:37:07 +0300

    upload to PyPI

--- src/upload_to_pypi.py
+++ src/upload_to_pypi.py
@@ -7,5 +7,6 @@ def upload_to_pypi():
     os.environ["INPUT_PYPI_TOKEN"]
     print(os.environ["GITHUB_REF"])
     sha = os.environ["GITHUB_SHA"]
+    print(sha)
     subprocess.check_output(shlex.split(f"git checkout {sha}"))
     print(subprocess.check_output(shlex.split("cat setup.cfg")))
