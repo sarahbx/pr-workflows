f8cca66d5848c93ccb242bda3da37d5606c68482
Meni Yakove <myakove@redhat.com>
2021-08-22 23:43:22 +0300

    upload to PyPI

--- src/upload_to_pypi.py
+++ src/upload_to_pypi.py
@@ -1,12 +1,16 @@
 import os
-import shlex
-import subprocess
+
+
+# import shlex
+# import subprocess
 
 
 def upload_to_pypi():
-    os.environ["INPUT_PYPI_TOKEN"]
-    print(os.environ["GITHUB_REF"])
-    sha = os.environ["GITHUB_SHA"]
-    print(sha)
-    subprocess.check_output(shlex.split(f"git checkout {sha}"))
-    print(subprocess.check_output(shlex.split("cat setup.cfg")))
+    for k, v in os.environ.items():
+        print(f"{k}: {v}")
+    # os.environ["INPUT_PYPI_TOKEN"]
+    # print(os.environ["GITHUB_REF"])
+    # sha = os.environ["GITHUB_SHA"]
+    # print(sha)
+    # subprocess.check_output(shlex.split(f"git checkout {sha}"))
+    # print(subprocess.check_output(shlex.split("cat setup.cfg")))
