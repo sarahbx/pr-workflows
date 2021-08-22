6705194135f84dc88942c2105bc3655e44073610
Meni Yakove <myakove@redhat.com>
2021-08-23 00:36:22 +0300

    upload to PyPI

--- src/upload_to_pypi.py
+++ src/upload_to_pypi.py
@@ -1,27 +1,29 @@
 import os
 import shlex
 import subprocess
+import sys
 
 import pygit2
 
 
 def upload_to_pypi():
-    print(subprocess.check_output(shlex.split("ls -la")))
-    print(subprocess.check_output(shlex.split("git branch")))
+    os.environ["TWINE_USERNAME"] = "__token__"
+    os.environ["TWINE_PASSWORD"] = os.environ["INPUT_PYPI_TOKEN"]
     tag = os.environ["GITHUB_REF"].split("/")[-1]
+    version = tag.strip("v")
+    dist_pkg = [pkg for pkg in os.listdir("dist") if version in pkg]
+    if not dist_pkg:
+        print("No package to upload under dist/ folder")
+        sys.exit(1)
+
+    dist_pkg = dist_pkg[0]
+    if version not in dist_pkg:
+        print(f"Package {dist_pkg} not match version {tag}")
+        sys.exit(1)
+
     repo = pygit2.Repository(".")
     repo.checkout(f"refs/remotes/origin/branch-{tag}")
-    print(subprocess.check_output(shlex.split("cat setup.cfg")))
-
-    # for k, v in os.environ.items():
-    #     print(f"{k}: {v}")
-    # os.environ["INPUT_PYPI_TOKEN"]
-    # print(os.environ["GITHUB_REF"])
-    # sha = os.environ["GITHUB_SHA"]
-    # print(sha)
-    # subprocess.check_output(shlex.split(f"git checkout {sha}"))
-    # print(subprocess.check_output(shlex.split("cat setup.cfg")))
-
-    # branch = f"branch-{tag}"
-    # subprocess.check_output(shlex.split(f"git checkout origin/{branch}"))
-    # print(subprocess.check_output(shlex.split("cat setup.cfg")))
+    subprocess.check_output(shlex.split(f"twine check dist/{dist_pkg}"))
+    subprocess.check_output(
+        shlex.split(f"twine upload dist/{dist_pkg} --skip-existing")
+    )
