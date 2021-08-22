670de34dec9244c06b59112f16ea79dcd55155d7
Meni Yakove <myakove@redhat.com>
2021-08-23 00:06:25 +0300

    upload to PyPI

--- src/upload_to_pypi.py
+++ src/upload_to_pypi.py
@@ -2,8 +2,15 @@ import os
 import shlex
 import subprocess
 
+import pygit2
+
 
 def upload_to_pypi():
+    tag = os.environ["GITHUB_REF"].split("/")[-1]
+    repo = pygit2.Repository(".git")
+    repo.checkout(f"refs/remotes/origin/branch-{tag}")
+    print(subprocess.check_output(shlex.split("cat setup.cfg")))
+
     # for k, v in os.environ.items():
     #     print(f"{k}: {v}")
     # os.environ["INPUT_PYPI_TOKEN"]
@@ -13,7 +20,6 @@ def upload_to_pypi():
     # subprocess.check_output(shlex.split(f"git checkout {sha}"))
     # print(subprocess.check_output(shlex.split("cat setup.cfg")))
 
-    tag = os.environ["GITHUB_REF"].split("/")[-1]
-    branch = f"branch-{tag}"
-    subprocess.check_output(shlex.split(f"git checkout origin/{branch}"))
-    print(subprocess.check_output(shlex.split("cat setup.cfg")))
+    # branch = f"branch-{tag}"
+    # subprocess.check_output(shlex.split(f"git checkout origin/{branch}"))
+    # print(subprocess.check_output(shlex.split("cat setup.cfg")))
