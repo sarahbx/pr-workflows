708be47dbcc2f4532368c9f72f5a5901f42a4d7f
Sarah Bennert <sbennert@redhat.com>
2021-08-23 11:37:45 -0400

    Semver labels

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,20 @@
+BLOCK_MERGE_SEMVER_CONTEXT = "Semver label"
 BLOCK_MERGE_VERIFY_CONTEXT = "Verified label"
+
 LABEL_VERIFIED = "Verified"
 LABEL_APPROVE = "Approve"
 NEEDS_MAINTAINERS_APPROVE = "Needs Maintainers Approve"
+
+LABEL_SEMVER_PREFIX = "Semver"
+LABELS_SEMVER_MAJOR = [
+    f"{LABEL_SEMVER_PREFIX}/Major",
+    f"{LABEL_SEMVER_PREFIX}/Breaking-Change",
+]
+LABELS_SEMVER_MINOR = [
+    f"{LABEL_SEMVER_PREFIX}/Minor",
+    f"{LABEL_SEMVER_PREFIX}/Non-Breaking-Feature",
+]
+LABELS_SEMVER_PATCH = [
+    f"{LABEL_SEMVER_PREFIX}/Patch",
+    f"{LABEL_SEMVER_PREFIX}/Non-Breaking-Fix",
+]
