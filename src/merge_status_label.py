0cc71306c8150ab87225f697baae0e5da0993bbd
Meni Yakove <myakove@redhat.com>
2021-08-29 18:53:32 +0300

    Add Merge status label action

--- src/merge_status_label.py
+++ src/merge_status_label.py
@@ -5,7 +5,7 @@ def merge_status_label(data, repo):
     label = "Ready for merge"
     commit = repo.get_commit(data["commit"]["sha"])
     pull = list(commit.get_pulls())[0]
-    if commit.get_combined_status() == "success":
+    if commit.get_combined_status().state == "success":
         add_label(pull=pull, label=label)
     else:
         if label in get_labels(pull=pull):
