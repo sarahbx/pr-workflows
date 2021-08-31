8f90f9ad79886da9d9e6d1f947537d2021a23675
Meni Yakove <myakove@redhat.com>
2021-08-31 15:17:40 +0300

    Issue from PR action

--- src/issue_from_pr.py
+++ src/issue_from_pr.py
@@ -1,7 +1,8 @@
 def issue_from_pr(repo, pull):
-    issue_title = f"{pull.title} #{pull.number}"
+    issue_title = pull.title
     try:
-        issue = repo.get_issue(issue_title)
+        repo.get_issue(issue_title)
     except AssertionError:
         issue = repo.create_issue(issue_title)
         issue.add_to_assignees(issue.user)
+        issue.create_comment(f"Address #{pull.number}")
