316c2d5ec4b7f4c4e169aeae0179b214066980bc
Meni Yakove <myakove@redhat.com>
2021-08-31 15:24:52 +0300

    Issue from PR action

--- src/issue_from_pr.py
+++ src/issue_from_pr.py
@@ -1,8 +1,12 @@
 def issue_from_pr(repo, pull):
     issue_title = pull.title
-    try:
-        repo.get_issue(issue_title)
-    except AssertionError:
+
+    def _issue_exists():
+        for _issue in repo.get_issues():
+            if _issue.title == issue_title:
+                return False
+
+    if not _issue_exists():
         issue = repo.create_issue(issue_title)
         issue.add_to_assignees(issue.user)
         issue.create_comment(f"Address #{pull.number}")
