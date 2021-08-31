6cb26ec82b7b09746997c38fd19bbcb4430382d1
Meni Yakove <myakove@redhat.com>
2021-08-31 16:55:08 +0300

    Issue from PR action

--- src/issue_from_pr.py
+++ src/issue_from_pr.py
@@ -1,15 +1,25 @@
+def _get_issue_title(pull):
+    return f"[ISSUE] {pull.title}"
+
+
+def _get_issue(repo, pull, issue_title):
+    for _issue in repo.get_issues():
+        if _issue.title == issue_title and _issue.state == "open":
+            print(f"{_issue.title} #{_issue.number} state: {_issue.state}")
+            print(f"Issue already exists for PR {issue_title} #{pull.number}")
+            return _issue
+
+
 def issue_from_pr(repo, pull):
-    issue_title = f"[ISSUE] {pull.title}"
-
-    def _issue_exists():
-        for _issue in repo.get_issues():
-            if _issue.title == issue_title and _issue.state == "open":
-                print(f"{_issue.title} #{_issue.number} state: {_issue.state}")
-                print(f"Issue already exists for PR {issue_title} #{pull.number}")
-                return True
-
-    if not _issue_exists():
+    issue_title = _get_issue_title(pull=pull)
+    if not _get_issue(repo=repo, pull=pull, issue_title=issue_title):
         print(f"Create issue for PR: {issue_title} #{pull.number}")
         issue = repo.create_issue(issue_title)
         issue.add_to_assignees(issue.user)
         issue.create_comment(f"Address #{pull.number}")
+
+
+def close_issue(repo, pull):
+    issue = _get_issue(repo=repo, pull=pull, issue_title=_get_issue_title(pull=pull))
+    print(f"Close issue {issue.title} #{issue.number}")
+    issue.edit(state="closed")
