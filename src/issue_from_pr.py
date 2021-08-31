d7192ff57f444cd0b442c79f26c46ed219443b1e
Meni Yakove <myakove@redhat.com>
2021-08-31 15:45:10 +0300

    Issue from PR action

--- src/issue_from_pr.py
+++ src/issue_from_pr.py
@@ -4,6 +4,7 @@ def issue_from_pr(repo, pull):
     def _issue_exists():
         for _issue in repo.get_issues():
             if _issue.title == issue_title and _issue.state == "open":
+                print(f"{_issue.title} #{_issue.number} state: {_issue.state}")
                 print(f"Issue already exists for PR {issue_title} #{pull.number}")
                 return True
 
