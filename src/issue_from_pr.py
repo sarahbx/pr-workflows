bf300919487248bc9fbbd161f1f862b00a5861a8
Meni Yakove <myakove@redhat.com>
2021-08-31 15:34:34 +0300

    Issue from PR action

--- src/issue_from_pr.py
+++ src/issue_from_pr.py
@@ -4,11 +4,12 @@ def issue_from_pr(repo, pull):
     def _issue_exists():
         for _issue in repo.get_issues():
             if _issue.title == issue_title:
-                print(f"Issue already exists for PR {issue_title}")
+                print(f"Issue already exists for PR {issue_title} #{pull.number}")
                 return False
         return True
 
     if not _issue_exists():
+        print(f"Create issue for PR: {issue_title} #{pull.number}")
         issue = repo.create_issue(issue_title)
         issue.add_to_assignees(issue.user)
         issue.create_comment(f"Address #{pull.number}")
