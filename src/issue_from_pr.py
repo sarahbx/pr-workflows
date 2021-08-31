8142e25627f9274cd736b01800ccead6c93bcf3c
Meni Yakove <myakove@redhat.com>
2021-08-31 15:08:22 +0300

    Issue from PR action

--- src/issue_from_pr.py
+++ src/issue_from_pr.py
@@ -1,10 +1,7 @@
-import contextlib
-
-
 def issue_from_pr(repo, pull):
-    with contextlib.suppress(AssertionError):
-        issue = repo.get_issue(pull.title)
-
-    if not issue:
-        issue = repo.create_issue(f"{pull.title} #{pull.number}")
+    issue_title = f"{pull.title} #{pull.number}"
+    try:
+        issue = repo.get_issue(issue_title)
+    except AssertionError:
+        issue = repo.create_issue(issue_title)
         issue.add_to_assignees(issue.user)
