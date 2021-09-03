56452afb6862e205d650dc71966e61aaede78c7b
Meni Yakove <myakove@redhat.com>
2021-09-04 02:47:47 +0300

    Remove label Ready for merge on push

--- src/remove_merge_checks.py
+++ src/remove_merge_checks.py
@@ -1,4 +1,4 @@
-from src.constants import LABEL_APPROVE, LABEL_VERIFIED
+from src.constants import LABEL_APPROVE, LABEL_VERIFIED, READY_FOR_MERGE
 from src.utils import (
     remove_label,
     set_commit_status_pending_no_approve,
@@ -10,5 +10,6 @@ def remove_merge_checks(pull):
     last_commit = list(pull.get_commits())[-1]
     remove_label(pull=pull, label=LABEL_VERIFIED)
     remove_label(pull=pull, label=LABEL_APPROVE)
+    remove_label(pull=pull, label=READY_FOR_MERGE)
     set_commit_status_pending_no_verify(commit=last_commit)
     set_commit_status_pending_no_approve(commit=last_commit)
