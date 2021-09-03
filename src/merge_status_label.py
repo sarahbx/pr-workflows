4340179787550c427e01697698275ce939c52a1c
Meni Yakove <myakove@redhat.com>
2021-09-03 23:52:37 +0300

    merge_status_label: dont fail in no commit

--- src/merge_status_label.py
+++ src/merge_status_label.py
@@ -2,6 +2,10 @@ from src.utils import add_label, get_labels, remove_label
 
 
 def merge_status_label(pull, commit):
+    if not commit:
+        print("No commit given")
+        return
+
     label = "Ready for merge"
     if commit.get_combined_status().state == "success":
         add_label(pull=pull, label=label)
