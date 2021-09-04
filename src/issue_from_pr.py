a4e6661db068599b2982479448ebcefde5ae777b
Meni Yakove <myakove@redhat.com>
2021-09-04 03:37:00 +0300

    issue from pr fix

--- src/issue_from_pr.py
+++ src/issue_from_pr.py
@@ -6,17 +6,17 @@ def _get_issue_info(issue):
     return f"{issue.title} #{issue.number} state: {issue.state}"
 
 
-def _get_issue(repo, pull, issue_title):
+def _get_issue(repo, pull):
+    issue_title = _get_issue_title(pull=pull)
     for _issue in repo.get_issues():
         if _issue.title == issue_title and _issue.state == "open":
             print(_get_issue_info(issue=_issue))
-            print(f"Issue already exists for PR {issue_title} #{pull.number}")
             return _issue
 
 
 def issue_from_pr(repo, pull):
     issue_title = _get_issue_title(pull=pull)
-    if not _get_issue(repo=repo, pull=pull, issue_title=issue_title):
+    if not _get_issue(repo=repo, pull=pull):
         print(f"Create issue for PR: {issue_title} #{pull.number}")
         issue = repo.create_issue(title=issue_title)
         issue.add_to_assignees(issue.user)
@@ -24,6 +24,6 @@ def issue_from_pr(repo, pull):
 
 
 def close_issue_from_pr(repo, pull):
-    issue = _get_issue(repo=repo, pull=pull, issue_title=_get_issue_title(pull=pull))
-    print(f"Closing issue {issue.title} #{issue.number}")
+    issue = _get_issue(repo=repo, pull=pull)
+    print(f"Closing issue {issue.title} #{issue.number} for pull {pull.number}")
     issue.edit(state="closed")
