65229a8f72798b74ad50ac10191624f0b90753f1
rnetser <rnetser@redhat.com>
2021-09-05 21:52:58 +0300

    Add generic set_commit_status

--- src/merge_status_label.py
+++ src/merge_status_label.py
@@ -7,9 +7,8 @@ def merge_status_label(pull, commit):
         print("No commit given")
         return
 
-    label = READY_FOR_MERGE
     if commit.get_combined_status().state == "success":
-        add_label(pull=pull, label=label)
+        add_label(pull=pull, label=READY_FOR_MERGE)
     else:
-        if label in get_labels(pull=pull):
-            remove_label(pull=pull, label=label)
+        if READY_FOR_MERGE in get_labels(pull=pull):
+            remove_label(pull=pull, label=READY_FOR_MERGE)
